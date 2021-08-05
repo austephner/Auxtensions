31bf3930b68c533b91efbc88e3751cf9cbfc81f2
Austin <austephner@gmail.com>
2021-08-05 16:47:06 -0400

    added initial float extensions, updated readme

---
+++ FloatExtensions.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+
+namespace Flintex
+{
+    /// <summary>
+    /// Float extensions which provide additional functionality for <c>float</c> typed variables.
+    /// </summary>
+    // todo: add function attributes similar to Mathf functions such as [FreeFunction(IsThreadSafe = true)]
+    public static class FloatExtensions
+    {
+        /// <summary>
+        ///     Rounds this <see cref="float"/> to an <see cref="int"/> using <see cref="Mathf.RoundToInt"/>.
+        /// </summary>
+        /// <param name="value">
+        ///     This <see cref="float"/> value.
+        /// </param>
+        /// <returns>
+        ///     A rounded <see cref="int"/>.
+        /// </returns>
+        /// <example>
+        ///     (1.8215f).RoundToInt()
+        /// </example>
+        public static int RoundToInt(this float value) => Mathf.RoundToInt(value);
+
+        /// <summary>
+        ///     Rounds this <see cref="float"/> down to an <see cref="int"/> using <see cref="Mathf.FloorToInt"/>.
+        /// </summary>
+        /// <param name="value">
+        ///     This <see cref="float"/> value.
+        /// </param>
+        /// <returns>
+        ///     A rounded <see cref="int"/>.
+        /// </returns>
+        /// <example>
+        ///     (1.8215f).FloorToInt()
+        /// </example>
+        public static int FloorToInt(this float value) => Mathf.FloorToInt(value);
+
+        /// <summary>
+        ///     Rounds this <see cref="float"/> up to an <see cref="int"/> using <see cref="Mathf.CeilToInt"/>.
+        /// </summary>
+        /// <param name="value">
+        ///     This <see cref="float"/> value.
+        /// </param>
+        /// <returns>
+        ///     A rounded <see cref="int"/>.
+        /// </returns>
+        /// <example>
+        ///     (1.8215f).CeilToInt()
+        /// </example>
+        public static int CeilToInt(this float value) => Mathf.CeilToInt(value);
+
+        /// <summary>
+        ///     Multiplies this <see cref="float"/> with itself, effectively squaring it.
+        /// </summary>
+        /// <param name="value">
+        ///     This <see cref="float"/> value.
+        /// </param>
+        /// <returns>
+        ///     A squared <see cref="float"/>.
+        /// </returns>
+        /// <example>
+        ///     (1.8215f).Square()
+        /// </example>
+        public static float Square(this float value) => value * value;
+
+        /// <summary>
+        ///     Raises this <see cref="float"/> to the given <see cref="power"/> using <see cref="Mathf.Pow"/>.
+        /// </summary>
+        /// <param name="value">
+        ///     This <see cref="float"/> value.
+        /// </param>
+        /// <param name="power">
+        ///     The power to raise this <see cref="float"/> value to.
+        /// </param>
+        /// <returns>
+        ///     A squared <see cref="float"/>.
+        /// </returns>
+        /// <example>
+        ///     (1.8215f).Pow(5)
+        /// </example>
+        public static float Pow(this float value, float power) => Mathf.Pow(value, power);
+
+        /// <summary>
+        ///     Calculates the square root of this <see cref="float"/> using <see cref="Mathf.Sqrt"/>.
+        /// </summary>
+        /// <param name="value">
+        ///     This <see cref="float"/> value.
+        /// </param>
+        /// <returns>
+        ///     The square of this <see cref="float"/> value.
+        /// </returns>
+        /// <example>
+        ///     (1.8215f).Sqrt()
+        /// </example>
+        public static float Sqrt(this float value) => Mathf.Sqrt(value);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# Flintex
-Helpful float and int extensions for Unity development.
+# Flintor
+Helpful C# `float`, `int`, and `Vector3`/`Vector2` extensions for Unity development.
+
+# Usage
+1. Import the Flintex namespace
+
+```c#
+using Flintex;
+```
+
+2. Access `int`, `float`, and `Vector2`/`Vector3` extension methods as needed.
+
+```c#
+// TODO: do something to an int
+var myInt = 100;
+Debug.Log(myInt.Something());
+
+// Round a float value to an integer.
+var myFloat = 1.71248f;
+Debug.Log(myFloat.RoundToInt());
+
+// Apply Mathf.Abs(...) to all values of the Vector3
+var myVector3 = new Vector3(-1, -23, 400);
+Debug.Log(myVector3.Abs());
+```
+
+3. ???
+4. Profit
+
+# Contribution Rules
+1. Code must follow formatting patterns of the existing codebase.
+2. Fully document and comment all functions. See below for an example.
+3. Code must go through pull requests before being merged. 
+4. Commits must list all changes to the codebase separated by commas
+
+## Documentation Example
+* Summary tag
+* Tags for all parameters
+* Uses "<see>" with a `cref` reference to other functions or code
+* Returns tag
+* Example tag
+```c#
+/// <summary>
+///     Rounds this <see cref="float"/> to an <see cref="int"/> using <see cref="Mathf.RoundToInt"/>.
+/// </summary>
+/// <param name="value">
+///     This <see cref="float"/> value.
+/// </param>
+/// <returns>
+///     A rounded <see cref="int"/>.
+/// </returns>
+/// <example>
+///     (1.8215f).RoundToInt()
+/// </example>
+public static int RoundToInt(this float value) => Mathf.RoundToInt(value);
+```
+
+The function would then appear like this within JetBrains Rider:
+![Documentation Example](https://i.imgur.com/4aeRhiw.png)
