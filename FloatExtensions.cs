86076b53232bb247fd355f9aba8ef5c58e799484
Austin <austephner@gmail.com>
2021-08-05 19:11:46 -0400

    renamed project from flintex to flintor, added quite a few more mathf functions to float extensions, updated readme

--- FloatExtensions.cs
+++ FloatExtensions.cs
@@ -1,41 +1,75 @@
 ﻿using UnityEngine;
 
-namespace Flintex
+namespace Flintor
 {
     /// <summary>
     /// Float extensions which provide additional functionality for <c>float</c> typed variables.
     /// </summary>
     // todo: add function attributes similar to Mathf functions such as [FreeFunction(IsThreadSafe = true)]
+    // todo: Mathf.Min, Mathf.Max, Mathf.LerpAngle, Mathf.LerpClamped
+    // todo: documentation
     public static class FloatExtensions
     {
+        #region Mathf Functions
+        
         /// <summary>
-        ///     Rounds this <see cref="float"/> to an <see cref="int"/> using <see cref="Mathf.RoundToInt"/>.
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static float Abs(this float value) => Mathf.Abs(value);
+        
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static float Acos(this float value) => Mathf.Acos(value);
+        
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="approximation"></param>
+        /// <returns></returns>
+        public static bool Approximately(this float value, float approximation) => Mathf.Approximately(value, approximation); 
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static float Asin(this float value) => Mathf.Asin(value);
+        
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static float Atan(this float value) => Mathf.Atan(value);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static float Atan2(this float value, float y) => Mathf.Atan2(value, y);
+
+        /// <summary>
+        ///     Rounds this <see cref="float"/> up using <see cref="Mathf.CeilToInt"/>.
         /// </summary>
         /// <param name="value">
         ///     This <see cref="float"/> value.
         /// </param>
         /// <returns>
-        ///     A rounded <see cref="int"/>.
+        ///     A rounded <see cref="float"/>.
         /// </returns>
         /// <example>
-        ///     (1.8215f).RoundToInt()
+        ///     (1.618f).CeilToInt()
         /// </example>
-        public static int RoundToInt(this float value) => Mathf.RoundToInt(value);
-
-        /// <summary>
-        ///     Rounds this <see cref="float"/> down to an <see cref="int"/> using <see cref="Mathf.FloorToInt"/>.
-        /// </summary>
-        /// <param name="value">
-        ///     This <see cref="float"/> value.
-        /// </param>
-        /// <returns>
-        ///     A rounded <see cref="int"/>.
-        /// </returns>
-        /// <example>
-        ///     (1.8215f).FloorToInt()
-        /// </example>
-        public static int FloorToInt(this float value) => Mathf.FloorToInt(value);
-
+        public static float Ceil(this float value) => Mathf.Ceil(value);
+        
         /// <summary>
         ///     Rounds this <see cref="float"/> up to an <see cref="int"/> using <see cref="Mathf.CeilToInt"/>.
         /// </summary>
@@ -46,24 +80,194 @@ namespace Flintex
         ///     A rounded <see cref="int"/>.
         /// </returns>
         /// <example>
-        ///     (1.8215f).CeilToInt()
+        ///     (1.618f).CeilToInt()
         /// </example>
         public static int CeilToInt(this float value) => Mathf.CeilToInt(value);
+        
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static float Clamp(this float value, float min, float max) => Mathf.Clamp(value, min, max);
 
         /// <summary>
-        ///     Multiplies this <see cref="float"/> with itself, effectively squaring it.
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static float Clamp01(this float value) => Mathf.Clamp01(value);
+        
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static float Cos(this float value) => Mathf.Cos(value);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static float DeltaAngle(this float value, float target) => Mathf.DeltaAngle(value, target);
+        
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static float Exp(this float value) => Mathf.Exp(value);
+        
+        /// <summary>
+        ///     Rounds this <see cref="float"/> down using <see cref="Mathf.FloorToInt"/>.
         /// </summary>
         /// <param name="value">
         ///     This <see cref="float"/> value.
         /// </param>
         /// <returns>
-        ///     A squared <see cref="float"/>.
+        ///     A rounded <see cref="float"/>.
         /// </returns>
         /// <example>
-        ///     (1.8215f).Square()
+        ///     (1.618f).FloorToInt()
         /// </example>
-        public static float Square(this float value) => value * value;
+        public static float Floor(this float value) => Mathf.Floor(value);
+        
+        /// <summary>
+        ///     Rounds this <see cref="float"/> down to an <see cref="int"/> using <see cref="Mathf.FloorToInt"/>.
+        /// </summary>
+        /// <param name="value">
+        ///     This <see cref="float"/> value.
+        /// </param>
+        /// <returns>
+        ///     A rounded <see cref="int"/>.
+        /// </returns>
+        /// <example>
+        ///     (1.618f).FloorToInt()
+        /// </example>
+        public static int FloorToInt(this float value) => Mathf.FloorToInt(value);
 
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="absMax"></param>
+        /// <param name="gamma"></param>
+        /// <returns></returns>
+        public static float Gamma(this float value, float absMax, float gamma) => Mathf.Gamma(value, absMax, gamma);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="to"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static float InverseLerpTo(this float value, float to, float time) => Mathf.InverseLerp(value, to, time);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="from"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static float InverseLerpFrom(this float value, float from, float time) => Mathf.InverseLerp(from, value, time);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns></returns>
+        public static float InverseLerpOver(this float value, float from, float to) => Mathf.InverseLerp(from, to, value);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="to"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static float LerpTo(this float value, float to, float time) => Mathf.Lerp(value, to, time);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="from"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static float LerpFrom(this float value, float from, float time) => Mathf.Lerp(from, value, time);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns></returns>
+        public static float LerpOver(this float value, float from, float to) => Mathf.Lerp(from, to, value);
+        
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static float Log(this float value) => Mathf.Log(value);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="base"></param>
+        /// <returns></returns>
+        public static float Log(this float value, float @base) => Mathf.Log(value, @base);
+        
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static float Log10(this float value) => Mathf.Log10(value);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="target"></param>
+        /// <param name="maxDelta"></param>
+        /// <returns></returns>
+        public static float MoveTowards(this float value, float target, float maxDelta) => Mathf.MoveTowards(value, target, maxDelta);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static float PerlinNoiseX(this float value, float y) => Mathf.PerlinNoise(value, y);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static float PerlinNoiseY(this float value, float x) => Mathf.PerlinNoise(x, value);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static float PingPong(this float value, float length) => Mathf.PerlinNoise(value, length);
+        
         /// <summary>
         ///     Raises this <see cref="float"/> to the given <see cref="power"/> using <see cref="Mathf.Pow"/>.
         /// </summary>
@@ -77,9 +281,99 @@ namespace Flintex
         ///     A squared <see cref="float"/>.
         /// </returns>
         /// <example>
-        ///     (1.8215f).Pow(5)
+        ///     (1.618f).Pow(5)
         /// </example>
         public static float Pow(this float value, float power) => Mathf.Pow(value, power);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static float Repeat(this float value, float length) => Mathf.Repeat(value, length);
+        
+        /// <summary>
+        ///     Rounds this <see cref="float"/> using <see cref="Mathf.RoundToInt"/>.
+        /// </summary>
+        /// <param name="value">
+        ///     This <see cref="float"/> value.
+        /// </param>
+        /// <returns>
+        ///     A rounded <see cref="float"/>.
+        /// </returns>
+        /// <example>
+        ///     (1.618f).Round()
+        /// </example>
+        public static float Round(this float value) => Mathf.Round(value);
+
+        /// <summary>
+        ///     Rounds this <see cref="float"/> to an <see cref="int"/> using <see cref="Mathf.RoundToInt"/>.
+        /// </summary>
+        /// <param name="value">
+        ///     This <see cref="float"/> value.
+        /// </param>
+        /// <returns>
+        ///     A rounded <see cref="int"/>.
+        /// </returns>
+        /// <example>
+        ///     (1.618f).RoundToInt()
+        /// </example>
+        public static int RoundToInt(this float value) => Mathf.RoundToInt(value);
+        
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static float Sign(this float value) => Mathf.Sign(value);
+        
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static float Sin(this float value) => Mathf.Sin(value);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="target"></param>
+        /// <param name="currentVelocity"></param>
+        /// <param name="smoothTime"></param>
+        /// <returns></returns>
+        public static float SmoothDamp(this float value, float target, ref float currentVelocity, float smoothTime) => Mathf.SmoothDamp(value, target, ref currentVelocity, smoothTime);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="target"></param>
+        /// <param name="currentVelocity"></param>
+        /// <param name="smoothTime"></param>
+        /// <param name="maxSpeed"></param>
+        /// <returns></returns>
+        public static float SmoothDamp(this float value, float target, ref float currentVelocity, float smoothTime, float maxSpeed) => Mathf.SmoothDamp(value, target, ref currentVelocity, smoothTime, maxSpeed);
+
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="target"></param>
+        /// <param name="currentVelocity"></param>
+        /// <param name="smoothTime"></param>
+        /// <param name="maxSpeed"></param>
+        /// <param name="deltaTime"></param>
+        /// <returns></returns>
+        public static float SmoothDamp(this float value, float target, ref float currentVelocity, float smoothTime, float maxSpeed, float deltaTime) => Mathf.SmoothDamp(value, target, ref currentVelocity, smoothTime, maxSpeed, deltaTime);
+        
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static float SmoothStep(this float value) => Mathf.Sin(value);
 
         /// <summary>
         ///     Calculates the square root of this <see cref="float"/> using <see cref="Mathf.Sqrt"/>.
@@ -91,8 +385,78 @@ namespace Flintex
         ///     The square of this <see cref="float"/> value.
         /// </returns>
         /// <example>
-        ///     (1.8215f).Sqrt()
+        ///     (1.618f).Sqrt()
         /// </example>
         public static float Sqrt(this float value) => Mathf.Sqrt(value);
+        
+        /// <summary>
+        /// todo: documentation
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static float Tan(this float value) => Mathf.Tan(value);
+
+        #endregion
+
+        #region Custom Extensions
+
+        /// <summary>
+        ///     Remaps this <see cref="float"/> relative to an "in range" and "out range". Advanced normalization.
+        /// </summary>
+        /// <param name="value">
+        ///     This <see cref="float"/> value.
+        /// </param>
+        /// <param name="inMin">
+        ///     The lower input range value.
+        /// </param>
+        /// <param name="inMax">
+        ///     The upper input range value.
+        /// </param>
+        /// <param name="outMin">
+        ///     The lower output range value.
+        /// </param>
+        /// <param name="outMax">
+        ///     The upper output range value.
+        /// </param>
+        /// <returns>
+        ///     A remapped <see cref="float"/>.
+        /// </returns>
+        /// <example>
+        ///     (1.618f).Remap(0.0f, 2.0f, 0.0f, 1.0f)
+        /// </example>
+        public static float Remap(this float value, float inMin, float inMax, float outMin, float outMax) => outMin + (value - inMin) * (outMax - outMin) / (inMax - inMin);
+
+        /// <summary>
+        ///     Checks to see if this <see cref="float"/> is "not a number" using <see cref="float.IsNaN"/>
+        /// </summary>
+        /// <param name="value">
+        ///     This <see cref="float"/> value.
+        /// </param>
+        /// <returns>
+        ///     Whether or not the <see cref="float"/> is a number.
+        /// </returns>
+        /// <example>
+        ///     (1.618f).IsNan()
+        /// </example>
+        public static bool IsNan(this float value) => float.IsNaN(value);
+
+        /// <summary>
+        ///     Rounds this <see cref="float"/> to a multiple of the given value.
+        /// </summary>
+        /// <param name="value">
+        ///     This <see cref="float"/> value.
+        /// </param>
+        /// <param name="multiple">
+        ///     The multiple for which to divide the value before rounding then multiply afterwards.
+        /// </param>
+        /// <returns>
+        ///     A <see cref="float"/> value that has been rounded to a multiple of the given value.
+        /// </returns>
+        /// <example>
+        ///     (1.618f).RoundToMultipleOf(5)
+        /// </example>
+        public static float RoundToMultipleOf(this float value, int multiple) => Mathf.Round(value / multiple) * multiple;
+
+        #endregion
     }
 }
--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # Flintor
+## Summary
 Helpful C# `float`, `int`, and `Vector3`/`Vector2` extensions for Unity development.
 
-# Usage
-1. Import the Flintex namespace
+## Features
+* Extensions for common types that use Unity's `Mathf` class
+* Additional extension functions that are useful
+* Full documentation including examples
+
+## Usage
+1. Download the repository into your `Assets` folder (if downloading from Git)
+2. Import the Flintex namespace
 
 ```c#
 using Flintex;
 ```
 
-2. Access `int`, `float`, and `Vector2`/`Vector3` extension methods as needed.
+3. Access `int`, `float`, and `Vector2`/`Vector3` extension methods as needed.
 
 ```c#
 // TODO: do something to an int
@@ -24,16 +31,17 @@ var myVector3 = new Vector3(-1, -23, 400);
 Debug.Log(myVector3.Abs());
 ```
 
-3. ???
-4. Profit
+4. ???
+5. Profit
 
-# Contribution Rules
-1. Code must follow formatting patterns of the existing codebase.
-2. Fully document and comment all functions. See below for an example.
+## Contribution Rules
+1. Code must follow formatting patterns of the existing codebase to retain consistency.
+2. <i>Try to</i> fully document and comment all functions. See below for an example.
 3. Code must go through pull requests before being merged. 
-4. Commits must list all changes to the codebase separated by commas
+4. Commits must list all changes to the codebase separated by commas.
+5. Alphabetize functions
 
-## Documentation Example
+### Documentation Example
 * Summary tag
 * Tags for all parameters
 * Uses "<see>" with a `cref` reference to other functions or code
@@ -57,3 +65,6 @@ public static int RoundToInt(this float value) => Mathf.RoundToInt(value);
 
 The function would then appear like this within JetBrains Rider: <br>
 ![Documentation Example](https://i.imgur.com/4aeRhiw.png)
+
+### Contributors & Credits
+* Austin Renner ([website](https://www.austephner.com/), [GitHub](https://github.com/austephner))
