7c00c6a22e5f75af40f634a2b9db555216627bcd
Austin <austephner@gmail.com>
2022-09-10 17:14:53 -0400

    Updated quaternion documentation

--- Runtime/QuaternionExtensions.cs
+++ Runtime/QuaternionExtensions.cs
@@ -4,7 +4,9 @@ namespace Auxtensions
 {
     public static class QuaternionExtensions
     {
-        // TODO: documentation
+        /// <summary> Removes all "NaN" values from this <see cref="Quaternion"/>. </summary>
+        /// <param name="value">This <see cref="Quaternion"/>.</param>
+        /// <returns>A new <see cref="Quaternion"/> without "NaN" values.</returns>
         public static Quaternion RemoveNaNs(this Quaternion value)
         {
             value.x = value.x.RemoveNaN();
