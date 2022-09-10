03ab74f273fbb6a212825b044ee7fa0e433fa5e1
Austin <austephner@gmail.com>
2022-09-10 17:47:42 -0400

    Reformatted object extensions documentation

--- Runtime/ObjectExtensions.cs
+++ Runtime/ObjectExtensions.cs
@@ -7,18 +7,10 @@ namespace Auxtensions
     /// </summary>
     public static class ObjectExtensions
     {
-        /// <summary>
-        /// Uses the <see cref="JsonUtility"/> class to quickly convert this <see cref="object"/> into JSON.
-        /// </summary>
-        /// <param name="object">
-        /// This <see cref="object"/> to convert to JSON.
-        /// </param>
-        /// <param name="prettyPrint">
-        /// When true, enables formatting for easily reading the result JSON.
-        /// </param>
-        /// <returns>
-        /// JSON <see cref="string"/>.
-        /// </returns>
+        /// <summary> Uses the <see cref="JsonUtility"/> class to quickly convert this <see cref="object"/> into JSON. </summary>
+        /// <param name="object"> This <see cref="object"/> to convert to JSON. </param>
+        /// <param name="prettyPrint"> When true, enables formatting for easily reading the result JSON. </param>
+        /// <returns> JSON <see cref="string"/>. </returns>
         public static string ToJson(this object @object, bool prettyPrint = false)
         {
             return JsonUtility.ToJson(@object, prettyPrint);
