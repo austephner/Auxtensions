30ca5ed22483feab4d1f7eac1382b0431aa19a1b
Austin <austephner@gmail.com>
2025-03-30 11:39:21 -0400

    Added some basic mono behaviour extensions accessible in-class from `this.InvokeWhenTrue(...)` due to the way C# recognizes these extension classes.

---
+++ Runtime/MonoBehaviourExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Auxtensions
+{
+    public static class MonoBehaviourExtensions
+    {
+        public static void InvokeWhenTrue<T>(this T monoBehaviour, Func<bool> condition, Action action) where T : MonoBehaviour
+        {
+            monoBehaviour.StartCoroutine(InvokeWhenTrue(condition, action));
+        }
+
+        private static IEnumerator InvokeWhenTrue(Func<bool> condition, Action action)
+        {
+            while (condition?.Invoke() == true) yield return new WaitForEndOfFrame();
+            action?.Invoke();
+        }
+
+        public static void InvokeNextFrame<T>(this T monoBehaviour, Action action) where T : MonoBehaviour
+        {
+            monoBehaviour.StartCoroutine(InvokeNextFrame(action));
+        }
+
+        private static IEnumerator InvokeNextFrame(Action action)
+        {
+            yield return new WaitForEndOfFrame();
+            action?.Invoke();
+        }
+    }
+}
